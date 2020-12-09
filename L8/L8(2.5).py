27b07ce8efd3427da59ef897a723b4a4f6e3fff7
KseniaMakara <73828804+KseniaMakara@users.noreply.github.com>
2020-12-09 13:06:42 +0200

    Update L8(2.5).py

--- L8/L8(2.5).py
+++ L8/L8(2.5).py
@@ -1,14 +1,17 @@
 import random
 n=int(input('Enter n = '))
 m=int(input('Enter m = '))
-a = [[random.randint(-100,100) for i in range(n)] for j in range(m)]
+a = [[float(input("a[{0}][{1}]=".format(i, j))) for j in range(n)] for i in range(n)]
+k=0
 def print_matrix(a):
     for el in a:
         print(el)
 print_matrix(a)
-k = 0
-for j in range(m):
-    for i in range(n):
-        if a[i][j]>0:
+def is_paralel(l1,l2):
+    return l1[0]/l2[0]==l1[1]/l2[1]
+
+for i in range(n-1):
+    for j in range(i+1,n):
+        if (is_paralel(a[i],a[j])):
             k+=1
 print(k)
