09bb6f410cb203ebe41152d03ce825f43b796d50
KseniaMakara <73828804+KseniaMakara@users.noreply.github.com>
2020-12-30 22:40:55 +0200

    Update L14(1.5).py

--- L14/L14(1.5).py
+++ L14/L14(1.5).py
@@ -1,103 +1,68 @@
 import random
 class TTriangle():
-    def __init__(self, a=0, b=0, c=0):
-        self.a = a
-        self.b = b
-        self.c = c
-    @property
-    def a(self):
-        return self.__a
-    @a.setter
-    def a(self, value):
-        if value >= 0:
-            self.__a = value
-        else:
-            raise Exception('a should be positive')
-    @property
-    def b(self):
-        return self.__b
-    @b.setter
-    def b(self, value):
-        if value >= 0:
-            self.__b = value
-        else:
-            raise Exception('a should be positive')
-    @property
-    def c(self):
-        return self.__c
-    @c.setter
-    def c(self, value):
-        if value >= 0:
-            self.__c = value
-        else:
-            raise Exception('c should be positive')
-    def print_a_b_c(self):
-        self.a = float(input(' Enter first side = '))
-        self.b = float(input(' Enter second side = '))
-        self.c = float(input(' Enter third side = '))
-        print(' First side is {0}, second side is {1}, third side is {2}'.format(self.a,self.b,self.c))
+
     def print_perimeter(self):
-        p = self.a+self.b + self.c
-        print(p)
+        return self.a+self.b + self.c
+
     def square(self):
         p = self.a + self.b + self.c
         h_p = p/2
-        s = (h_p(h_p-self.a)(h_p-self.b)(h_p-self.c))**1/2
-        print(s)
-class ETriangle(TTriangle):
-    def __init__(self, a):
-        super().__init__(a)
-    def square(self):
-        sq = (self.a**2*3**1/2)/4
-        return sq
+        return (h_p(h_p-self.a)(h_p-self.b)(h_p-self.c))**1/2
 
-class ITriangle(TTriangle):
-    def __init__(self, a, b ):
-        super().__init__(a,b)
+class ETriangle(TTriangle): # рівносторонній
+    def __init__(self, a=0):
+        self.a = a
+    def print_a_b_c(self):
         self.a = float(input(' Enter first side = '))
-        self.b = float(input(' Enter second side = '))
-        print(a,a,b)
-    def print_perimeter(self):
-        p = 3*self.a
-        print(p)
+        print(a)
 
-class RTriangle():
+    def print_perimeter(self):
+        return 3*self.a
+
+    def square(self):
+        p = 2*self.a + self.b
+        h_p = p/2
+        return (3**1/2)*self.a**2/4
+
+class ITriangle(TTriangle): #рівнобедрений
     def __init__(self, a=0, b=0):
-        super().__init__(a, b)
+        self.a = a
+        self.b = b
     def print_a_b_c(self):
         self.a = float(input(' Enter first side = '))
         self.b = float(input(' Enter second side = '))
-        c = (self.a ** 2 + self.b ** 2) ** 1 / 2
-        print(' First side is {0}, second side is {1}, third side is {2}'.format(self.a,self.b, c))
+
+    def print_perimeter(self):
+        return 2*self.a+self.b
+
     def square(self):
-        ss = self.a*self.b/2
-        return ss
+        p = 2*self.a + self.b
+        h_p = p/2
+        return (h_p*(h_p-self.a)*(h_p-self.b)*(h_p-self.a))**1/2
 
-t1 = ETriangle(3)
-t2 = ETriangle(6)
-t3 = RTriangle(5,6)
-t4 = RTriangle(6,7)
-t5 = ITriangle(4,3)
-t6 = ITriangle(7,9)
+class RTriangle(TTriangle): #прямокутний
+    def __init__(self, a=0, b=0):
+        self.a = a
+        self.b = b
+    def print_a_b_c(self):
+        self.a = float(input(' Enter first side = '))
+        self.b = float(input(' Enter second side = '))
 
-tt=t1
-tt.square()
-tt=t2
-tt.square()
-tt=t3
-tt.square()
-tt=t4
-tt.square()
-perimeter = t5
-perimeter.print_perimeter()
-perimeter = t6
-perimeter.print_perimeter()
+    def print_perimeter(self):
+        return self.a+self.b+((self.a)**2+(self.b)**2)**1/2
 
-m = []
-h = []
-n = random.randint(1,6)
-for i in range(n):
-    m.append(tt)
-    h.append(perimeter)
-print(sum(m),sum(h))
+    def square(self):
+        return self.a*self.b/2
+
+classes = [ETriangle, ITriangle, RTriangle]
+objects = [classes[random.randint(0,2)](random.randint(3,25),random.randint(3,25)) for el in range(100)]
+sum_eq = 0
+sum_other = 0
+for obj in objects:
+    if obj.__class__ == ITriangle:
+        sum_eq+=obj.print_perimeter()
+    else:
+        sum_other+=obj.square()
+print(sum_eq,sum_other)
+
 
