0fa9a61e64c434620f4aaa9c2176a4bc6e0c965f
KseniaMakara <73828804+KseniaMakara@users.noreply.github.com>
2020-12-30 22:13:07 +0200

    Update L13(1.5).py

--- L13/L13(1.5).py
+++ L13/L13(1.5).py
@@ -1,40 +1,82 @@
-import random
-class Rectangle():
-    def __init__(self, x1=0, x2=0, x3 = 0, x4 = 0, y1 = 0, y2 = 0, y3 = 0, y4 = 0):
-        self.x1 = x1
-        self.x2 = x2
-        self.x3 = x3
-        self.x4 = x4
-        self.y1 = y1
-        self.y2 = y2
-        self.y3 = y3
-        self.y4 = y4
-    def print_x_y(self):
-        self.x1 = random.randint(-5,5)
-        self.x2 = random.randint(-5,5)
-        self.x3 = random.randint(-5,5)
-        self.x4 = random.randint(-5,5)
-        self.y1 = random.randint(-5,5)
-        self.y2 = random.randint(-5,5)
-        self.y3 = random.randint(-5,5)
-        self.y4 = random.randint(-5,5)
-
-    def calc_a(self):
-        return ((self.x1 - self.x2)**2 + (self.y1 - self.y2)**2)**1/2
-
-    def calc_b(self):
-        return ((self.x3 - self.x4)**2 + (self.y3 - self.y4)**2)**1/2
-
-    def __getitem__(self, key, value):
-        if key == 1:
-            return self.a
-        elif key == 2:
-            return self.b
+class TRTriangle():
+    def __init__(self, a=0):
+        self.a = a
+    @property
+    def a(self):
+        return self.__a
+    @a.setter
+    def a(self, value):
+        if value >= 0:
+            self.__a = value
         else:
-            raise Exception('Wrong key')
-    def squre(self):
-        return self.calc_a()*self.calc_b()
+            raise Exception('a should be positive')
+    def print_a(self):
+        self.a = float(input(' Enter first side = '))
+        print(self.a)
+    def print_perimeter(self):
+        return 3*self.a
+    def square(self):
+        p = self.a*3
+        h_p = p/2
+        return self.a*3**(1/3)/4
 
-    def perimeter(self):
-        return  2*(self.calc_a()+self.calc_b())
+    def __lt__(self, other):
+        return self.square()<other.square()
 
+    def __eq__(self, other):
+        return self.square() == other.square()
+
+    def __gt__(self, other):
+        return self.square() > other.square()
+
+    def __le__(self, other):
+        return self.square() <= other.square()
+
+    def __ge__(self, other):
+        return self.square() >= other.square()
+
+    def __add__(self, other):
+        return TRTriangle(other.a + self.a)
+
+    def __sub__(self, other):
+        return TRTriangle(self.a - other.a)
+
+    def __mul__(self, other):
+        return TRTriangle(other * self.a)
+
+class TPiramid(TRTriangle):
+    def __init__(self, a=0, h=0):
+        super().__init__(a)
+        self.h = h
+
+    def __add__(self, other):
+        return TPiramid(other.a + self.a, self.h + other.h)
+
+    def __sub__(self, other):
+        return TPiramid(self.a - other.a, self.h - other.h )
+
+    def __mul__(self, other):
+        return TPiramid(other * self.a, other*self.h)
+
+    def print_h(self):
+        self.h = float(input('Enter hight = '))
+        print(self.h)
+
+    def square(self):
+        p = self.a*3
+        h_p = p/2
+        l = float(input('enter l='))
+        return self.square()+l*self.print_perimeter()/2
+
+    def v(self):
+        return super().square() * self.h/3
+
+
+#______________________________________
+t = TRTriangle()
+t.print_a()
+t.print_perimeter()
+t.square()
+pyramid=TPiramid()
+pyramid.print_h()
+pyramid.v()
