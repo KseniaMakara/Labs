06a159ea0f3e03564c95aa7714bb2e414d051777
KseniaMakara <73828804+KseniaMakara@users.noreply.github.com>
2020-12-30 22:10:56 +0200

    Update L12(3.5).py

--- L12/L12(3.5).py
+++ L12/L12(3.5).py
@@ -18,39 +18,22 @@ class Rectangle():
         self.y2 = random.randint(-5,5)
         self.y3 = random.randint(-5,5)
         self.y4 = random.randint(-5,5)
-    def sides(self):
-        a1 = ((self.x1-self.x2)**2 + (self.y1 - self.y2)**2)**1/2
-        a2 = ((self.x2-self.x3)**2 + (self.y2 - self.y2)**3)**1/2
-        b1 = ((self.x3-self.x4)**2 + (self.y3 - self.y4)**2)**1/2
-        b2 = ((self.x4-self.x1)**2 + (self.y4 - self.y1)**2)**1/2
-        @property
-        def compare_sides(self):
-            if a1 == a2 and b1 == b2:
-                a = a1
-                b = b1
-                print('It is a rectangle')
-            elif a1 == b1 and b2 == a2:
-                a = a1
-                b = b2
-                print('It is a rectangle')
-            elif a1 == b2 and a2 == b1:
-                a = a1
-                b = b1
-                print('It is a rectangle')
-            else:
-                raise Exception('Another figure')
 
-            def __getitem__(self, key, value):
-                if key == 1:
-                    return self.a
-                elif key == 2:
-                    return self.b
-                else:
-                    raise Exception('Wrong key')
-            def squre(self):
-                s = a*b
-                print(s)
-            def perimeter(self):
-                p = 2*(a+b)
-                print(p)
+    def calc_a(self):
+        return ((self.x1 - self.x2)**2 + (self.y1 - self.y2)**2)**1/2
 
+    def calc_b(self):
+        return ((self.x3 - self.x4)**2 + (self.y3 - self.y4)**2)**1/2
+
+    def __getitem__(self, key, value):
+        if key == 1:
+            return self.a
+        elif key == 2:
+            return self.b
+        else:
+            raise Exception('Wrong key')
+    def squre(self):
+        return self.calc_a()*self.calc_b()
+
+    def perimeter(self):
+        return  2*(self.calc_a()+self.calc_b())
