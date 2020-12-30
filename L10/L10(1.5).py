25df91278bde01c452919a96f3dab7ab14c2ae73
KseniaMakara <73828804+KseniaMakara@users.noreply.github.com>
2020-12-30 18:52:36 +0200

    Update L10(1.5).py

--- L10/L10(1.5).py
+++ L10/L10(1.5).py
@@ -1,41 +1,21 @@
-class Number():
-    def __init__(self, n):
+class TNumber():
+    def init(self, n=0, m=0):
         self.numbers = n
+        self.m = list(map(int, list(str(n))))
     def enter_n(self):
         self.numbers = input('Enter n = ')
-    def get_digits(self):
-        print(len(self.numbers.split(',')))
-    def get_number(self):
+        self.m = list(map(int, list(str(self.numbers))))
+        return self.numbers
+
+    def printn(self):
         print(self.numbers)
-    def count_n(self):
-        el = int(input("Element in massive = "))
-        b = self.numbers.split(',')
-        g = 0
-        for i in range(len(self.numbers.split(','))):
-            if el == int(b[i]):
-                g+=1
-        print(g)
-    def get_sum(self):
-        s=0
-        for i in range(len(self.numbers.split(','))):
-            a = self.numbers.split(',')
-            s+=int(a[i])
-        print(s)
-    def __gt__(self, other):
-        if len(s)!=len(p):
-            if len(s)>len(p):
-                return True
-            else:
-                return False
-        return False
 
-    # def get_len(self):
-    #     return len(self.digits)
-#_________________________________________________
-p = Number('5,6,3,6,9,4')
-s = Number('8,4,6,3')
-p.get_number()
-p.get_sum()
-p.count_n()
+    def count(self):
+        k = int(input('k = '))
+        return self.m.count(k)
 
+    def sum(self):
+        return sum(self.m)
 
+    def gt(self, other):
+        return self.numbers < other.numbers
