179a97e1fbb4d44462d17cf561f87b5d213bf95e
KseniaMakara <73828804+KseniaMakara@users.noreply.github.com>
2020-12-30 22:06:57 +0200

    Update L10(2.5).py

--- L10/L10(2.5).py
+++ L10/L10(2.5).py
@@ -5,11 +5,11 @@ class Date():
         self.month = _month
         self.year = _year
 class Traveller():
-    def __init__(self, _surname_and_initials, _profession,_day_of_birth, _month_of_birth, _year_of_birth):
+    def __init__(self, _day, _month, _year, _surname_and_initials, _profession,_day_of_birth, _month_of_birth, _year_of_birth, _country, _region, _city_or_village, _street, _house, _term, _date):
         self.surname_and_initials = _surname_and_initials
         self.profession = _profession
-        self.day_of_birth = Date
-        self.adress = Adress_of_traveller
+        self.day_of_birth = Date(_day, _month, _year)
+        self.adress = Adress_of_traveller( _country, _region, _city_or_village, _street, _house, _term, _date)
 
 class Adress_of_traveller():
     def __init__(self, _country, _region, _city_or_village, _street, _house, _term, _date):
@@ -19,20 +19,16 @@ class Adress_of_traveller():
         self.street = _street
         self.house = _house
         self.term = _term
-        self.info = Traveller
+        c = list(map(int, _date.split('.')))
+        self.date = datetime.date(*c)
+
     def get_terms(self):
         allowed_to_stay = int(input('enter days of staying = '))
-        current = datetime.datetime.today()
+        current = int(input('current days of staying : '))
         terms = current - self.term
         if terms < allowed_to_stay:
             return "can stay"
         def get_rest(self):
             return allowed_to_stay - terms
-
-
-
-
-
-
-
-
+t= Traveller(8,5,6,6,5,5,5,5,5,5,55,5,5,5,'12.8.20')
+print(t.adress.date)
